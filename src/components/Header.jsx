2e8eb2887b5f2b85dd2c130e57820bf7e3fadb5c
Rodrigofms <rodrigoms.rec@gmail.com>
2024-10-04 08:09:04 -0300

    documentacao e estilizacao

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,29 @@
-function Header() {
+import "../App.css"; // Certifique-se de importar seu CSS
+
+/* eslint react/prop-types: 0 */
+const Header = ({ changeTheme }) => {
   return (
     <header className="header">
       <h1>AutoCar</h1>
+      <div className="quadrado-temas">
+        <div
+          className="quadrado-tema laranja"
+          onClick={() => changeTheme("laranja")}
+          title="Tema Laranja"
+        ></div>
+        <div
+          className="quadrado-tema roxo"
+          onClick={() => changeTheme("roxo")}
+          title="Tema Roxo"
+        ></div>
+        <div
+          className="quadrado-tema verde"
+          onClick={() => changeTheme("verde")}
+          title="Tema Verde"
+        ></div>
+      </div>
     </header>
   );
-}
+};
 
 export default Header;
